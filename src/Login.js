ddc6886fc0fd947b947284165c207994eee70ff5
RTK <120491428@qq.com>
2017-04-20 16:58:24 +0800

    init

--- src/Login.js
+++ src/Login.js
@@ -3,22 +3,54 @@
  */
 import React, {Component} from 'react';
 import {Button} from 'antd';
+import TweenOne from 'rc-tween-one';
+import SvgDrawPlugin from 'rc-tween-one/lib/plugin/SvgDrawPlugin';
+import PathPlugin from 'rc-tween-one/lib/plugin/PathPlugin';
+TweenOne.plugins.push(SvgDrawPlugin);
+TweenOne.plugins.push(PathPlugin);
 
 export default class Test1 extends Component {
-
-    componentDidMount(){
+    componentDidMount() {
     }
 
     render() {
-        const { from } = this.props.location.state || { from: { pathname: '/' } }
-
+        const {from} = this.props.location.state || {from: {pathname: '/'}}
         return (
-            <div className="App" style={{backgroundColor:'rgb(0, 204, 0)',fontSize:36,color:'#fff'}}>
+            <div className="App" style={{fontSize: 36}}>
                 test113
                 <p>ddd</p>
-                <Button type="primary" onClick={() => this.props.history.push('/test2',{test:'from test1'})}>
+                <Button type="primary" onClick={() => this.props.history.push('/test2', {test: 'from test1'})}>
                     Go Test2
                 </Button>
+                <svg width="200" height="84" version="1.2"
+                     style={{display: 'block', margin: 'auto'}}
+                >
+                    <TweenOne
+                        animation={{
+                            SVGDraw: '40% 60%',
+                            yoyo: true,
+                            repeat: -1,
+                            duration: 1000
+                        }}
+                        style={{fill: 'none', strokeWidth: 10, stroke: '#019BF0'}}
+                        component="path"
+                        d="M10,10c65,50,115,50,180,0"/>
+                </svg>
+                <div style={{position:'relative'}}>
+                    <TweenOne
+                        animation={{
+                            path: 'M5 75 C 35 5, 75 20,105 100 S 145 145, 175 75',
+                            yoyo: true,
+                            repeat: -1,
+                            duration: 6000
+                        }}
+                        style={{fill: 'none', strokeWidth: 10, stroke: '#019BF0',position:'absolute'}}>
+                        <div style={{position:'absolute',width:'20px',height:'20px',borderRadius:'10px',backgroundColor:'blue'}}></div>
+                    </TweenOne>
+                    <svg width="100%" height="200" version="1.2">
+                        <path d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80" stroke="black" fill="transparent"/>
+                    </svg>
+                </div>
                 {
                     from.pathname === '/' ?
                         null
